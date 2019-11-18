615da42e81013fb39ac9b062ffb0df843e28dd4a
Swina Allen <swina.allen@gmail.com>
2019-11-18 20:22:29 +0300

    Fixed demo

--- src/plugins/rockiot.radial.js
+++ src/plugins/rockiot.radial.js
@@ -12,7 +12,7 @@ export default (function(global, factory) {
       global.Gauge = Gauge;
     }
   })(typeof window === "undefined" ? this : window, function(global, undefined) {
-  
+
     var document = global.document,
       slice = Array.prototype.slice,
       requestAnimationFrame = (global.requestAnimationFrame ||
@@ -22,7 +22,7 @@ export default (function(global, factory) {
           function(cb) {
             return setTimeout(cb, 1000 / 60);
           });
-  
+
     // EXPERIMENTAL!!
     /**
      * Simplistic animation function for animating the gauge. That's all!
@@ -48,13 +48,13 @@ export default (function(global, factory) {
             if ((pos/=0.5) < 1) return 0.5*Math.pow(pos,3);
             return 0.5 * (Math.pow((pos-2),3) + 2);
           };
-  
+
       function animate() {
-        var progress = currentIteration / iterations, 
+        var progress = currentIteration / iterations,
             value = change * easing(progress) + start;
         step(value, currentIteration);
         currentIteration += 1;
-  
+
         if(progress < 1) {
           requestAnimationFrame(animate);
         }
@@ -62,17 +62,17 @@ export default (function(global, factory) {
       // start!
       requestAnimationFrame(animate);
     }
-  
-  
-  
+
+
+
     var Gauge = (function() {
       var SVG_NS = "http://www.w3.org/2000/svg";
-  
+
       var GaugeDefaults = {
         centerX: 50,
         centerY: 50
       };
-  
+
       var defaultOptions = {
         dialRadius: 45,
         offset: 10,
@@ -108,7 +108,7 @@ export default (function(global, factory) {
           return parseFloat(val).toFixed(precision);
         }
       };
-  
+
       function shallowCopy(/* source, ...targets*/) {
         var target = arguments[0], sources = slice.call(arguments, 1);
         sources.forEach(function(s) {
@@ -120,7 +120,7 @@ export default (function(global, factory) {
         });
         return target;
       }
-  
+
       /**
        * A utility function to create SVG dom tree
        * @param {String} name The SVG element name
@@ -133,7 +133,7 @@ export default (function(global, factory) {
         for(var attrName in attrs) {
           elem.setAttribute(attrName, attrs[attrName]);
         }
-  
+
         if(children) {
           children.forEach(function(c) {
             elem.appendChild(c);
@@ -141,7 +141,7 @@ export default (function(global, factory) {
         }
         return elem;
       }
-  
+
       /**
        * Translates percentage value to angle. e.g. If gauge span angle is 180deg, then 50%
        * will be 90deg
@@ -149,19 +149,19 @@ export default (function(global, factory) {
       function getAngle(percentage, gaugeSpanAngle) {
         return percentage * gaugeSpanAngle / 100;
       }
-  
+
       function normalize(value, min, limit) {
-        
+
         var val = Number(value);
         if ( Number(value) < 0 ){
           val += min
         }
-        console.log ( 'normalize animated=>',val , min , limit + min)
+        //console.log ( 'normalize animated=>',val , min , limit + min)
         if(val > limit) return limit;
         //if(val < min) return min;
         return val;
       }
-  
+
       function getValueInPercentage(value, min, max) {
         var mFactor = 1
         if ( min <  0 ){
@@ -172,7 +172,7 @@ export default (function(global, factory) {
         // var absMin = Math.abs(min);
         // return 100 * (absMin + value) / (max + absMin);
       }
-  
+
       /**
        * Gets cartesian co-ordinates for a specified radius and angle (in degrees)
        * @param cx {Number} The center x co-oriinate
@@ -188,7 +188,7 @@ export default (function(global, factory) {
           y: Math.round((cy + radius * Math.sin(rad)) * 1000) / 1000
         };
       }
-  
+
       // Returns start and end points for dial
       // i.e. starts at 135deg ends at 45deg with large arc flag
       // REMEMBER!! angle=0 starts on X axis and then increases clockwise
@@ -200,7 +200,7 @@ export default (function(global, factory) {
             start: getCartesian(cx, cy, radius, startAngle)
         };
       }
-  
+
       /**
        * Creates a Gauge object. This should be called without the 'new' operator. Various options
        * can be passed for the gauge:
@@ -265,19 +265,19 @@ export default (function(global, factory) {
           startAngle = endAngle;
           endAngle = tmp;
         }
-  
+
         function pathString(radius, startAngle, endAngle, largeArc) {
           var coords = getDialCoords(radius, startAngle, endAngle),
               start = coords.start,
               end = coords.end,
               largeArcFlag = typeof(largeArc) === "undefined" ? 1 : largeArc;
-          
+
           return [
-            "M", start.x, start.y, 
+            "M", start.x, start.y,
             "A", radius, radius, 0, largeArcFlag, 1, end.x, end.y
           ].join(" ");
         }
-  
+
         function initializeGauge(elem) {
           gaugeValueElem = svg("text", {
             x: 50,
@@ -315,7 +315,7 @@ export default (function(global, factory) {
           })
 
           gaugeUnitsElem.append ( units )
-          
+
 
           gaugeValuePath = svg("path", {
             "class": valueDialClass,
@@ -324,8 +324,8 @@ export default (function(global, factory) {
             "stroke-width": 2.5,
             d: pathString(radius, startAngle, startAngle) // value of 0
           });
-          
-          
+
+
 
           var angle = getAngle(100, 360 - Math.abs(startAngle - endAngle))
           var flag = angle <= 180 ? 0 : 1;
@@ -337,17 +337,17 @@ export default (function(global, factory) {
             "stroke-width": 2,
             d: pathString(radius, startAngle, endAngle, flag)
           })
-          
-        
+
+
           var gaugeNeedle =  svg('circle',{
             "class" : "rockiot-scale scale rockiot-needle-circle needle-circle",
             cx: 50,
             cy: 50,
             r: 2,
           })
-          
+
           var gaugeElement = svg("svg", {"viewBox": viewBox || "0 0 100 100", "class": gaugeClass},
-            [ 
+            [
               gaugeDialEl,
               gaugeValuePath,
               //gaugeValueElem,
@@ -355,8 +355,8 @@ export default (function(global, factory) {
               gaugeUnitsElem,
             ]
           );
-          
-          
+
+
           elem.appendChild(gaugeElement);
 
           if ( needle ){
@@ -364,7 +364,7 @@ export default (function(global, factory) {
           }
 
           if ( displayScale ){
-          
+
             gaugeScale = svg("g",{
               "class": scaleClass,
               stroke: "#eee"
@@ -381,7 +381,7 @@ export default (function(global, factory) {
             }
             if ( opts.min > 0 ){
               scaleOffsetNumber = opts.min
-              limit -= Math.abs(min) 
+              limit -= Math.abs(min)
             }
             for ( var n=0 ; n < (ticks*10)+1 ; n++ ){
                 var yT = 50-opts.dialRadius+(opts.dialRadius/10)
@@ -403,12 +403,12 @@ export default (function(global, factory) {
                     y2: yT+dT,
                     "stroke": scaleColor,
                     "stroke-width":.4,
-                    transform :'rotate(' + ( (n* factor) + startTick) + ' 50 50)' 
+                    transform :'rotate(' + ( (n* factor) + startTick) + ' 50 50)'
                   })
-                  
+
                   var numberOffset = 0
                   if ( n % 10 === 0 ){
-                    
+
                     if ( n === 0 && (startAngle - endAngle) === 1 ){
                       numberOffset = 2
                     }
@@ -420,9 +420,9 @@ export default (function(global, factory) {
                       y: yT-1,
                       "class" : "scaleNumbers",
                       fill: scaleColor,
-                      transform :'rotate(' + ( (n* factor) + startTick) + ' 50 50)' 
+                      transform :'rotate(' + ( (n* factor) + startTick) + ' 50 50)'
                     })
-                    
+
                     tickNr.append( parseFloat(n*(limit/ticks/10)+parseInt(scaleOffsetNumber)).toFixed(0))
                     gaugeScale.appendChild(tickNr)
                   }
@@ -430,7 +430,7 @@ export default (function(global, factory) {
                   gaugeScale.appendChild(tickEl)
                   gaugeElement.appendChild(gaugeScale)
                 }
-                
+
             }
           }
         }
@@ -466,7 +466,7 @@ export default (function(global, factory) {
               var s = startAngle+(i*tick)
               var angle = Math.PI/33*(i+tick)
               //var angle = (Math.PI/(360/fs/2)) * i
-              console.log ( 'x=>' , radius * Math.cos(angle) )
+              //console.log ( 'x=>' , radius * Math.cos(angle) )
               //console.log ( Math.PI , angle , radius )
               var txtNumber = svg('text',{
                 x : ((radius+offset) * Math.cos(angle)) + 50,
@@ -478,14 +478,14 @@ export default (function(global, factory) {
             }
           }
         }
-        
+
         function updateGauge(theValue, frame) {
           var lm = limit
           var mn = min
-          
+
           //if ( opts.min < 0 ){
           //  theValue += Math.abs(opts.min)
-            
+
           //}
           if ( opts.min > 0 ){
             mn = opts.min
@@ -494,7 +494,7 @@ export default (function(global, factory) {
           if ( opts.min < 0 ){
             mn = opts.min
           }
-          
+
           var val = getValueInPercentage(theValue, mn, lm),
               // angle = getAngle(val, 360 - Math.abs(endAngle - startAngle)),
               angle = getAngle(val, 360 - Math.abs(startAngle - endAngle)),
@@ -504,20 +504,20 @@ export default (function(global, factory) {
           var parent = document.querySelector('.gauge-' + serial)
           if ( needle ){
             drawNeedle()
-          }  
+          }
           if(displayValue) {
             gaugeValueElem.textContent = parseFloat(theValue).toFixed(precision)// label.call(opts,parseFloat(theValue).toFixed(precision));
           }
         }
 
 
-  
-        function setGaugeColor(value, duration) {        
-          var c = gaugeColor(value), 
+
+        function setGaugeColor(value, duration) {
+          var c = gaugeColor(value),
               dur = duration * 1000,
               pathTransition = "stroke " + dur + "ms ease";
               // textTransition = "fill " + dur + "ms ease";
-  
+
           gaugeValuePath.style.stroke = c;
           gaugeValuePath.style["-webkit-transition"] = pathTransition;
           gaugeValuePath.style["-moz-transition"] = pathTransition;
@@ -531,18 +531,18 @@ export default (function(global, factory) {
           ].join(";");
           */
         }
-  
+
         instance = {
           setMaxValue: function(max) {
             limit = max;
           },
           setValue: function(val) {
-           
-           
+
+
             value = val
             /*if ( min > 0 ){
               var lm = opts.max-min
-              
+
               value = (lm*val)/100
               console.log ('set value of % =>' , val, min , lm , ' = ' , value )
             }
@@ -551,7 +551,7 @@ export default (function(global, factory) {
               value += min
             }
 
-            console.log ( 'set value=> ', value , min , limit )
+            //console.log ( 'set value=> ', value , min , limit )
             //value = normalize(val, min, limit);
             if(gaugeColor) {
               setGaugeColor(value, 0)
@@ -569,7 +569,7 @@ export default (function(global, factory) {
             if(oldVal === value) {
               return;
             }
-  
+
             if(gaugeColor) {
               setGaugeColor(value, duration);
             }
@@ -586,17 +586,17 @@ export default (function(global, factory) {
             return value;
           }
         };
-  
+
         initializeGauge(gaugeContainer);
         instance.setValue(value);
         if ( needle ){
           drawNeedle()
-          
-        }  
+
+        }
         return instance;
       };
     })();
-  
+
     return Gauge;
   });
 
